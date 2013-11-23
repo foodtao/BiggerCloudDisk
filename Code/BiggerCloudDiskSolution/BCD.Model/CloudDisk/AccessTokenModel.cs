0a5f0bef4fad322f36a6c6542af4597210e62e43
135f2l <zhiyuan.ding@gmail.com>
2013-11-23 11:26:09 +0800

    新增token数据模型

---
+++ Code/BiggerCloudDiskSolution/BCD.Model/CloudDisk/AccessTokenModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BCD.Model.CloudDisk
+{
+    /// <summary>
+    /// 网盘oAuth认证后返回的读写权限令牌
+    /// 2013-11-23 by 丁智渊
+    /// </summary>
+    public class AccessTokenModel
+    {
+        /// <summary>
+        /// 网盘类型
+        /// </summary>
+        public CloudDiskType DiskType { get; set; }
+
+        /// <summary>
+        /// 返回的完整的字符串
+        /// </summary>
+        public string FullText { get; set; }
+
+        /// <summary>
+        /// 令牌
+        /// </summary>
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        /// 过期时间(单位秒)
+        /// </summary>
+        public string Expire { get; set; }
+    }
+}
