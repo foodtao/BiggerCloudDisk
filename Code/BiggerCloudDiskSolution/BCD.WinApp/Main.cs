8c727f8a527d82a925dece647e1665ec10a5b70e
foodtao <food0517@163.com>
2013-11-23 15:09:04 +0800

    main方法

--- Code/BiggerCloudDiskSolution/BCD.WinApp/Main.cs
+++ Code/BiggerCloudDiskSolution/BCD.WinApp/Main.cs
@@ -9,15 +9,14 @@ using System.Windows.Forms;
 
 namespace BCD.WinApp
 {
-    using System.IO;
-    using Dokan;
     using BCD.FilSystem;
 
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+    using Dokan;
 
+    public partial class Main : Form
+    {
+        public Main()
+        {
             DokanOptions options = new DokanOptions();
             options.ThreadCount = 1;
             options.DebugMode = true;
@@ -27,20 +26,6 @@ namespace BCD.WinApp
             DokanNet.DokanMain(options, new MirrorDisk("G:\\temp"));
 
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            //File.Create("D:\\Temp\\a.txt");
-
-            //MessageBox.Show(Path.GetExtension("D:\\Temp\\a.txt"));
-            //MessageBox.Show(Path.GetExtension("D:\\Temp"));
-
-            MessageBox.Show(Path.GetFileName("D:\\temp1\\1.txt"));
-            //Directory.Move("D:\\temp1","D:\\temp");
-
-
-
         }
     }
 }
