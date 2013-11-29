b4c90e4c8f401ea5afca58d54ed7916e43a3c12b
135f2l <zhiyuan.ding@gmail.com>
2013-11-29 22:52:49 +0800

    finish BaiduDiskApi

---
+++ Code/BiggerCloudDiskSolution/BCD.DiskInterface/Baidu/BaiduSignatureHelper.cs
@@ -0,0 +1,20 @@
+﻿using BCD.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace BCD.DiskInterface.Baidu
+{
+    public static class BaiduSignatureHelper
+    {
+        public static string GetSignature(string content, string secretKey)
+        {
+            string result = "";
+            result = HashHelper.HashStr(content, secretKey);
+            result = HttpUtility.UrlEncode(result);
+            return result;
+        }
+    }
+}
